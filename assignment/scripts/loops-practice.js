48ddb79ef643be33c9f95623e13fc3e3b3b01370
Oscar Carbajal <oscar.carbajal.work@gmail.com>
2022-06-19 22:02:33 -0500

    loop practice question 1b count 3-5

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -19,7 +19,11 @@ for (let i=0; i<6; i++){
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
+//change the index (starting point)
 console.log('count from 3 to 5');
+for (let i=3; i<6; i++){
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
